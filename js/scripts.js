0080f0401253f09f34725d819fb95c54a16ab1e2
GiuliaMenna <72873975+GiuliaMenna@users.noreply.github.com>
2022-09-09 12:04:20 +0200

    Ingrandimento immagini

--- js/scripts.js
+++ js/scripts.js
@@ -117,3 +117,31 @@ var oursScene = new ScrollMagic.Scene({
 .setClassToggle('.future', 'fade-in') //add class to future developments section
 .addTo(controller);
 })
+//INGRANDIMENTO MAPPE
+$('.zoom').click(function() {
+
+   var imageUrl = $(this).attr('src');
+   var imageHeight = $(this).prop('naturalHeight');
+   var windowHeight = $(window).height();
+
+   if (imageHeight > windowHeight) {
+     var optionalProperty = '; background-size: contain';
+   } else {
+     var optionalProperty = '';
+   }
+$('body').prepend('<div class="zoomOpen"><div style="background-image: url(' + imageUrl + ')' + optionalProperty + '"></div></div>');
+$('html').css('overflow', 'hidden');
+$('.zoomOpen').animate({
+      opacity: 1,
+      zoom: 1
+      }, 100, function() {
+         $(this).click(function() {
+            $(this).animate({
+               opacity: 0,
+               zoom: .85
+            }, 250, function() {
+            $(this).remove();
+         })
+      })
+   })
+})
